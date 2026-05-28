41c6b0d2db2b0f5997178486c72dd786441e4f47
Javed Ali <optionalaccc12@gmail.com>
2026-05-28 15:25:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>React Deployment with GitHub</h1>
+      <p>Welcome to React Deployment with GitHub</p>
+    </div>
+  )
+}
+
+export default App
